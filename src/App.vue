08a15dc4202612a4f87b0166ca392c336a44a1c4
rakhmight <mr.rakhimov@mail.com>
2023-03-28 17:47:45 +0500

    added auth page UI

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 </template>
 
 <script>
-console.log(process.env.VUE_APP_CLIENT_TOKEN)
+//console.log(process.env.VUE_APP_CLIENT_TOKEN)
 export default {
   name: 'App',
 
@@ -16,3 +16,31 @@ export default {
   }),
 }
 </script>
+
+<style>
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+
+body{
+    width: 100%;
+    height: 100%;
+    background-color: var(--bg-color);
+    position: relative;
+}
+
+html{
+    overflow: hidden;
+}
+
+/*=========================| VARS |===========================*/
+:root {
+  --main-color: #0d5fd8;
+  --bg-special-color: #0c2242;
+  --text-special-color: #7baef9;
+  --bg-color: #D9E3E3;
+}
+/*=============================================================*/
+</style>
